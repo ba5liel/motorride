8a89cdb305fecb87533af166a8b4f71248c3b309
Selamu <baslielselamu2018@gmail.com>
2020-06-30 04:49:37 +0300

    polishing

--- lib/config/configs.dart
+++ lib/config/configs.dart
@@ -1,4 +1,4 @@
 class Config{
   static String websocketurl = "https://2d29296e6c7d.ngrok.io/";
-  static String googleMapApiKey = "AIzaSyCQcLxm1sU5ZszNeYwawT19PzWMqEu--hs";
+  static String googleMapApiKey = "API KEY";
 }
